c8777c4a58eaaaefa7bbb5481be0111d2ef1a8b0
Rishab Mehra <rishab@stanford.edu>
2024-07-27 23:23:16 +0100

    Don't wrap about text

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -14,13 +14,11 @@ const About: React.FC = () => {
         I am the CTO and co-founder of <HighlightAlt>Pinnacle</HighlightAlt>, a London based AI venture.
       </p>
       <p>
-      My objective in life is to innovate and develop products <br />
-      that can directly impact significant populations. I am particularly <br />
-      interested in Computer Vision, Natural Language Processing and <br />
-      their consumer applications. In the past, I have researched in <br />
-      these fields at Stanford, and published in Nature, MLHC and NeurIPS. <br />
-      I have also delivered multiple features at Apple in these fields, which <br />
-      billions use daily, and filed patents related to them.
+      My objective in life is to innovate and develop products that can directly impact significant populations.
+      I am particularly interested in Computer Vision, Natural Language Processing and their consumer applications.
+      In the past, I have researched in these fields at Stanford, and published in Nature, MLHC and NeurIPS.
+      I have also delivered multiple features at Apple in these fields, which billions use daily,
+      and filed patents related to them.
       </p>
     </AboutWrapper>
   );
